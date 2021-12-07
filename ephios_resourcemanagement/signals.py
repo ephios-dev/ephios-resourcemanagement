086dc2c184c3adf78a4216d91df33e10f277e010
Julian B <jeriox@users.noreply.github.com>
2021-12-07 18:40:59 +0100

    refactor nav link

--- ephios_resourcemanagement/signals.py
+++ ephios_resourcemanagement/signals.py
@@ -7,6 +7,8 @@ from ephios.core.signals import nav_link
     dispatch_uid="ephios_resourcemanagement.register_nav_link",
 )
 def register_nav_link(sender, **kwargs):
-    return {
-        "Resources": "#"
-    }
+    return [{
+        "label": "Resources",
+        "url": "#",
+        "active": False,
+    }]
